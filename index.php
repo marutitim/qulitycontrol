fa5a769f31462d567d6abd9ad46d8a12de88f43f
marutitim <simiyuutim@gmail.com>
2016-09-14 14:08:26 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+
+<html>
+    <head>
+        <title>The Query::Home</title>
+        <link rel="shortcut icon" href="view/imgs/mysql1.png"  type="image/x-icon" >
+        <meta charset="windows-1252">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" type="text/css" href="/plugin/dhtmlx/dhtmlx4.5/skins/web/dhtmlx.css"/> 
+        <link rel="stylesheet" type="text/css" href="/plugin/dhtmlx/dhtmlx4.5/skins/terrace/dhtmlx.css"/> 
+        <link rel="stylesheet" type="text/css" href="view/css/fontawesome/css/font-awesome.min.css"/> 
+        <script src="/plugin/jquery/jquery-2.2.1.min.js"></script>
+        <script src="/plugin/dhtmlx/dhtmlx4.5/codebase/dhtmlx.js"></script>
+        <style>
+            html, body {
+                width: 100%;
+                height: 100%;
+                margin: 0px;
+                overflow: hidden;
+            }
+
+            .fa
+            {
+                color:#3da0e3; 
+            }
+            .bcf-toolbar-icons
+            {
+                color:#3da0e3; 
+            }
+            div.gridbox_dhx_terrace.gridbox table.hdr td div.hdrcell {
+                line-height: 36px;
+                padding-left: 20px;
+                text-transform: none !important;
+                width: auto;
+            }
+            div.gridbox_dhx_web.gridbox table.obj.row20px tr.rowselected td {
+                background-color: rgb(225, 244, 255) !important;
+                border-right-color: #fff !important;
+            }
+            div.gridbox_dhx_web.gridbox table.obj tr.rowselected td.cellselected {
+                background-color: #85d3ff;
+                color: #000;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <script src="view/js/main.js"></script>        
+        </div>
+    </body>
+</html>
